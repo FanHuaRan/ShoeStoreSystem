8937e4189d90b5328cdd49242a0762d2727f61d3
FanHuaRan <834171100@qq.com>
2017-01-21 17:23:45 +0800

    2016/1/21 fhr first

---
+++ Web/AspMvc/ShoeStoreWeb/ShoeStoreWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ShoeStoreWeb.Startup))]
+namespace ShoeStoreWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
